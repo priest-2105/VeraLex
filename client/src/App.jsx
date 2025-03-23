15b53eb3afe74163b5aba0445598a53c645fc81a
priest-2105 <priest2105@gmail.com>
2025-03-23 16:03:50 +0100

    react router setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,10 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import SubmitCase from "./pages/SubmitCase";
-import LawyerDashboard from "./pages/LawyerDashboard";
+import Home from "./pages/public/homepage/index";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/submit-case" element={<SubmitCase />} />
-      <Route path="/lawyer-dashboard" element={<LawyerDashboard />} />
     </Routes>
   );
 }
